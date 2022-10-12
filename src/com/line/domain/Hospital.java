97e579f61a821f3e9a962dd7f11ed70a6f96bcbf
seoyun <may-primavera@naver.com>
2022-10-12 15:51:56 +0900

    feat : hospital 필드 추가

--- src/com/line/domain/Hospital.java
+++ src/com/line/domain/Hospital.java
@@ -3,6 +3,12 @@ package com.line.domain;
 public class Hospital {
 
     private String id;
+    private String address;
+    private String district;
+    private String category;
+    private Integer emergencyRoom;
+    private String name;
+    private String subdivision;
 
     public Hospital(String id) {
         this.id = id;
